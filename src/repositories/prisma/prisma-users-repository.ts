aa2bfb6bd83eb692125e438e2d90a47ebc2a8781
Marcos Souza <marcos.souza@beeleads.com.br>
2023-10-21 13:18:53 -0300

    feat: add test

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,26 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+  async create({ name, email, password_hash }: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password_hash,
+      },
+    })
+
+    return user
+  }
+}
